549e0ee8705b7fe903b753c7ea47cbcf346b8bf3
CB-Pantomime <christopherbrown.contact@gmail.com>
2022-06-11 15:15:42 -0700

    discarded unwanted react factory files. refactored index.js to use class Main extending React.Component. rendering Main to page.

---
+++ my-app/src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <h1>hallow whirled</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
